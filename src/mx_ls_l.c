9c9e00a1f28adb4eb5cf084706c6dee78fec04ae
Oleh <geriys12@gmail.com>
2020-01-21 01:02:09 +0200

    Almost ready -l

--- src/mx_ls_l.c
+++ src/mx_ls_l.c
@@ -1,35 +1,33 @@
 #include "uls_header.h"
 
-const char *getUserName() {
-  uid_t uid = geteuid();
-  struct passwd *pw = getpwuid(uid);
-  
-  if (pw) {
-    return pw->pw_name;
-  }
+static const char *getUserName() {
+    uid_t uid = geteuid();
+    struct passwd *pw = getpwuid(uid);
 
-  return "";
+    if (pw)
+        return pw->pw_name;
+    return "";
 }
 
-void *getGroupName(char *argv) {
-  struct stat info;
-  lstat(argv, &info);
-  struct group *grp = getgrgid(info.st_gid);
+static void *getGroupName(char *file_name) {
+    struct stat info;
+    lstat(file_name, &info);
+    struct group *grp = getgrgid(info.st_gid);
 
-  if (grp)
-    return mx_strdup(grp->gr_name);
-  else
-  {
-    return mx_itoa(info.st_gid);
-  }
-  
+    if (grp)
+        return mx_strdup(grp->gr_name);
+    else
+        return mx_itoa(info.st_gid);
 }
 
-int mx_ls_l(char *argv) {
-    struct stat fileStat;
+static void edit_time(char *t) {
+    for(int i = 4; i < 16; i++) {
+        mx_printchar(t[i]);
+    }
+}
 
-    if(stat(argv,&fileStat) < 0)    
-        return 1;
+static void print_frst(struct stat fileStat) {
+
     mx_printstr( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
     mx_printstr( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
     mx_printstr( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
@@ -40,18 +38,26 @@ int mx_ls_l(char *argv) {
     mx_printstr( (fileStat.st_mode & S_IROTH) ? "r" : "-");
     mx_printstr( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
     mx_printstr( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    mx_printstr("  ");
+}
 
-    mx_printchar('\t');
+int mx_ls_l(char *file_name) {
+    struct stat fileStat;
 
-    mx_printstr(getUserName());
-    mx_printchar('\t');
-    mx_printstr(getGroupName(argv));
-    mx_printchar('\t');
+    if(stat(file_name, &fileStat) < 0)    
+        return 1;
+    print_frst(fileStat);
     mx_printint(fileStat.st_nlink);
-    mx_printchar('\t');
+    mx_printstr(" ");
+    mx_printstr(getUserName());
+    mx_printstr("  ");
+    mx_printstr(getGroupName(file_name));
+    mx_printstr("  ");
     mx_printint(fileStat.st_size);
-    mx_printchar('\t');
-    mx_printstr(argv);
-    mx_printchar('\n');
+    mx_printstr(" ");
+    edit_time(ctime(&fileStat.st_mtime)); 
+    mx_printstr("\t");
+    mx_printstr(file_name);
+    mx_printstr("\n");
     return 0;
 }
