a1d26d570e46501be478f13829c8ad1d90fb114c
Oleh <geriys12@gmail.com>
2020-01-20 13:46:22 +0200

    Some done whith -l

--- src/mx_ls_l.c
+++ src/mx_ls_l.c
@@ -1,47 +1,57 @@
 #include "uls_header.h"
 
+const char *getUserName() {
+  uid_t uid = geteuid();
+  struct passwd *pw = getpwuid(uid);
+  
+  if (pw) {
+    return pw->pw_name;
+  }
+
+  return "";
+}
+
+void *getGroupName(char *argv) {
+  struct stat info;
+  lstat(argv, &info);
+  struct group *grp = getgrgid(info.st_gid);
+
+  if (grp)
+    return mx_strdup(grp->gr_name);
+  else
+  {
+    return mx_itoa(info.st_gid);
+  }
+  
+}
+
 int mx_ls_l(char *argv) {
     struct stat fileStat;
 
     if(stat(argv,&fileStat) < 0)    
         return 1;
-    printf("File Size: \t\t%lld bytes\n",fileStat.st_size);
-    printf("Number of Links: \t%d\n",fileStat.st_nlink);
-    printf("File inode: \t\t%llu\n",fileStat.st_ino);
-    // printf("%ld.",fileStat.st_atimespec);
-    // mx_printstr("\n");
-    mx_printint(fileStat.st_blksize);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_blocks);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_dev);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_flags);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_gen);
-    mx_printstr("\n");
-    printf("%s",fileStat.st_birthtimespec);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_mode);
-    mx_printstr("\n");
-    //mx_printint(fileStat.st_qspare);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_rdev);
-    mx_printstr("\n");
-    mx_printint(fileStat.st_uid);
-    mx_printstr("\n");
-    mx_printstr("\n");
-    printf("File Permissions: \t");
-    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
-    printf("\n\n");
+    mx_printstr( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    mx_printstr( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    mx_printstr( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    mx_printstr( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    mx_printstr( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    mx_printstr( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    mx_printstr( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    mx_printstr( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    mx_printstr( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    mx_printstr( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+
+    mx_printchar('\t');
+
+    mx_printstr(getUserName());
+    mx_printchar('\t');
+    mx_printstr(getGroupName(argv));
+    mx_printchar('\t');
+    mx_printint(fileStat.st_nlink);
+    mx_printchar('\t');
+    mx_printint(fileStat.st_size);
+    mx_printchar('\t');
+    mx_printstr(argv);
+    mx_printchar('\n');
     return 0;
 }
